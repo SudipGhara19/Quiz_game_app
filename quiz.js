a3ac14e96b55db225507a3d55957a0f716d84b99
SudipGhara19 <sudipghara135@gmail.com>
2024-03-04 19:54:02 +0530

    display the Question and the option in the app

--- quiz.js
+++ quiz.js
@@ -7,3 +7,19 @@ const questionObj =
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
+
+
+const {answers, question, correctAnswer} = questionObj;
+
+const questionEl = document.getElementById("question");
+const scoreEl = document.getElementById("score");
+const optionEl = document.getElementById("options");
+
+questionEl.textContent = questionObj.question;
+
+for(let i = 0; i<answers.length; i++){
+  const optionBtn = document.createElement("button");
+  optionBtn.id = i + 1;
+  optionBtn.textContent = answers[i];
+  optionEl.appendChild(optionBtn);
+}
