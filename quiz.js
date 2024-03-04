4b90e0d78d4785d122d10838da27a89f9e48ae4a
SudipGhara19 <sudipghara135@gmail.com>
2024-03-04 20:28:42 +0530

    eventListener added to the option buttons and after answering turning fields empty

--- quiz.js
+++ quiz.js
@@ -3,23 +3,40 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
+      answers: ['Two', 'Three', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
 
 
-const {answers, question, correctAnswer} = questionObj;
+const {correctAnswer, answers, question} = questionObj;
+let score = 0;
 
 const questionEl = document.getElementById("question");
 const scoreEl = document.getElementById("score");
 const optionEl = document.getElementById("options");
 
+//showing the question
 questionEl.textContent = questionObj.question;
 
-for(let i = 0; i<answers.length; i++){
-  const optionBtn = document.createElement("button");
-  optionBtn.id = i + 1;
-  optionBtn.textContent = answers[i];
-  optionEl.appendChild(optionBtn);
-}
+//showing all answer option
+answers.forEach((opt)=> {
+  const btn = document.createElement("button");
+  btn.textContent = opt;
+  optionEl.appendChild(btn);
+
+  //event handling on the btn
+  btn.addEventListener('click', () => {
+    if(opt === correctAnswer){
+      score++;
+    }else{
+      score = score - 0.25;
+    }
+
+    scoreEl.textContent = `Score: ${score}`;
+
+    //after answered turning the fields empty
+    questionEl.textContent = "Quiz Completed";
+    optionEl.textContent = ''; 
+  });
+});
